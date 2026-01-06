36d8314abb59a33f1aa9e0277aee8a3af9b57b4a
qqx-d <m.dimalebedev@gmail.com>
2026-01-06 16:48:26 +0200

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Set base path for GitHub Pages deployment under repository name
+  base: '/Learn-Latvial-App-2/',
   plugins: [react()],
 })
 
